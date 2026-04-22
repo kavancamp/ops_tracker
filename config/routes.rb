4c07342074518fe9cf3106be2f8b2f27080af918
kavancamp <keenahvancampenhout@gmail.com>
2026-04-22 13:09:07 -0500

    Add edit and update routes for locations and assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "locations#index"
 
-  resources :locations, only: [ :index, :new, :create, :show ]
-  resources :assets, path: "equipment", only: [ :index, :new, :create, :show ]
+  resources :locations, only: [ :index, :new, :create, :show, :edit, :update ]
+  resources :assets, path: "equipment", only: [ :index, :new, :create, :show, :edit, :update ]
 end
